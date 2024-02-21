7806c837269d08db6b404f523879d23cbb2aaa94
Pradist Keawborisut <pradist.ke@gmail.com>
2024-02-21 08:18:45 +0700

    feat: add hello api

---
+++ src/main/java/com/pradist/user/HelloController.java
@@ -0,0 +1,15 @@
+package com.pradist.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+  @GetMapping
+  public String hello() {
+    return "Hello, This is a REST endpoint!";
+  }
+}
